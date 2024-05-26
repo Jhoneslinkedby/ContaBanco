be7efe6432997592f772c59e5222b30a6e0e67d0
Jhones Correa - Linkedby <jhones.correa@controlinformatica.com.br>
2024-05-26 18:42:57 -0300

    Implementei os métodos de entrada e exibição de mensagem para o usuário.

--- dio-desafio-sintaxe/src/ContaTerminal.java
+++ dio-desafio-sintaxe/src/ContaTerminal.java
@@ -1,29 +1,86 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
+    private int numero = 0;
+    private String agencia = "";
+    private String nome = "";
+    private double saldo = 0.0;
+
+    public ContaTerminal(){
+
+    }
+
+    public ContaTerminal(int numero, String agencia, String nome){
+        numero = this.numero;
+        agencia = this.agencia;
+        nome = this.nome;
+        saldo = 0;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public String getAgencia(){
+        return agencia;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setAgencia(String agencia) {
+         this.agencia = agencia;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+
+    public void depositar(double valor) {
+         setSaldo(getSaldo() + valor);
+    }
+
+    public String mensagemContaCriada() {
+        String mensagemConta = "Olá " + getNome() + ", " +
+                "obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + getAgencia() + ", " +
+                "conta "+getNumero()+" e seu saldo "+getSaldo()+" já está disponível para saque";
+        return mensagemConta;
+    }
+
+
+
     public static void main(String[] args) throws Exception {
-        int numero = 0;
-        String agencia = "";
-        String nome = "";
-        double saldo = 0.0;
-
         Scanner scanner = new Scanner(System.in);
+        ContaTerminal conta1 = new ContaTerminal();
 
-        //Solicita a agencia
         System.out.println("Por favor, digite o número da conta: ");
-        numero = scanner.nextInt();
+        conta1.setNumero(scanner.nextInt());
 
         System.out.println("Por favor, digite a agencia: ");
-        agencia = scanner.next();
+        conta1.setAgencia(scanner.next());
 
         System.out.println("Por favor, digite o seu nome: ");
-        nome = scanner.next();
+        conta1.setNome(scanner.next());
+
+        System.out.println("Por favor, digite o valor que deseja depositar: ");
+        conta1.depositar(scanner.nextDouble());
 
 
-        System.out.println("Olá " + nome + ", " +
-                "obrigado por criar uma conta em nosso banco, " +
-                "sua agência é " + agencia + ", " +
-                "conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+
+        System.out.println(conta1.mensagemContaCriada());
+
+
+
+
+
 
     }
 }
